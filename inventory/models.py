e476fb472f19a1aead70b6153c6df9d2ac0e001e
Aidon Lebar <aidonlebar@gmail.com>
2017-10-01 22:04:17 -0400

    database models and schema made

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,26 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+class InventoryItem(models.Model):
+    item_name = models.CharField(max_length = 100)
+    total_stock = models.IntegerField()
+
+    def __str__(self):
+        return self.item_name
+
+class Order(models.Model):
+    borrower_name = models.CharField(max_length = 100)
+    start_time = models.DateTimeField('Start Time')
+    end_time = models.DateTimeField('End Time')
+    order_created = models.DateTimeField('Created', auto_now_add = True)
+    order_last_modified = model.DateTimeField('Last Modified', auto_now = False)
+
+class OrderItem(models.Model):
+    order = model.ForeignKey(Order, on_delete=models.CASCADE)
+    item = model.ForeignKey(InventoryItem, on_delete=models.CASCADE)
+    quantity_borrowed = models.IntegerField()
+    quantity_returned = models.IntegerField()
+
+    def __str__(self):
+        return '{} {}'.format(self.quantity_borrowed, self.item.item_name)
