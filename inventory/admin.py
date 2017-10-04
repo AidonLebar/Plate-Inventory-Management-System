39e6d21d3cee7ed6264b2cdf6da3525bd20072d8
Aidon Lebar <aidonlebar@gmail.com>
2017-10-04 17:24:42 -0400

    added active orders/order items  and a rudimentary up-to-date stock calculation

--- inventory/admin.py
+++ inventory/admin.py
@@ -2,19 +2,30 @@ from django.contrib import admin
 from .models import InventoryItem, Order, OrderItem
 
 
+class InventoryItemAdmin(admin.ModelAdmin):
+    fields = ['item_name', 'total_stock',]
+    list_display = ('item_name', 'total_stock', 'current_stock')
+    ordering = ['item_name']
+
+    def current_stock(self, obj):
+        return obj.currentStock()
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
 
-class InventoryItemAdmin(admin.ModelAdmin):
-    fields = ['item_name', 'total_stock']
-
 class OrderAdmin(admin.ModelAdmin):
     fieldsets = [
-    (None,          {'fields' : ['borrower_name','order_created', 'order_last_modified']}),
+    (None,          {'fields' : ['borrower_name','order_created', 'order_last_modified', 'active']}),
     ('Information', {'fields' : ['start_time', 'end_time']},),
     ]
-    readonly_fields = ('order_created', 'order_last_modified')
+    readonly_fields = ('order_created', 'order_last_modified', 'active')
     inlines = [OrderItemInline]
+    list_display = ['borrower_name', 'start_time', 'end_time', 'active']
+    ordering = ['order_created']
+    list_filter =['order_created']
+
+    def active(self, obj):
+        return obj.activeOrder()
 
 
 admin.site.register(Order, OrderAdmin)
--- inventory/models.py
+++ inventory/models.py
@@ -1,4 +1,5 @@
 import datetime
+from django.utils import timezone
 
 from django.db import models
 
@@ -8,6 +9,15 @@ class InventoryItem(models.Model):
 
     def __str__(self):
         return self.item_name
+
+    def currentStock(self): #calculates current stock as total - currently lent out
+        stock = self.total_stock
+        order_item_set = self.orderitem_set.all()
+        for  i in order_item_set :
+            if i.activeOrderItem():
+                stock = stock - i.quantity_borrowed
+
+        return stock
 
 class Order(models.Model):
     borrower_name = models.CharField(max_length = 100)
@@ -20,6 +30,10 @@ class Order(models.Model):
         start_date = self.start_time.strftime('%Y-%m-%d')
         return '{} : {}'.format(self.borrower_name, start_date)
 
+    def activeOrder(self):
+        now = timezone.now()
+        return (now >= self.start_time) and (now <= self.end_time)
+
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     item = models.ForeignKey(InventoryItem, on_delete=models.CASCADE)
@@ -28,3 +42,6 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return '{} {}'.format(self.quantity_borrowed, self.item.item_name)
+
+    def activeOrderItem(self):
+        return self.order.activeOrder();
