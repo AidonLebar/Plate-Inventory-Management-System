9763011cb344faf695379b4cccb8cd310fcb1c92
Aidon Lebar <aidonlebar@gmail.com>
2017-10-04 16:23:46 -0400

    added admin site, minor url changes, models integer field changed to positive integer field, order model __str__

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import InventoryItem, Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+class InventoryItemAdmin(admin.ModelAdmin):
+    fields = ['item_name', 'total_stock']
+
+class OrderAdmin(admin.ModelAdmin):
+    fieldsets = [
+    (None,          {'fields' : ['borrower_name','order_created', 'order_last_modified']}),
+    ('Information', {'fields' : ['start_time', 'end_time']},),
+    ]
+    readonly_fields = ('order_created', 'order_last_modified')
+    inlines = [OrderItemInline]
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(InventoryItem, InventoryItemAdmin)
--- inventory/models.py
+++ inventory/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class InventoryItem(models.Model):
     item_name = models.CharField(max_length = 100)
-    total_stock = models.IntegerField()
+    total_stock = models.PositiveIntegerField()
 
     def __str__(self):
         return self.item_name
@@ -14,13 +14,17 @@ class Order(models.Model):
     start_time = models.DateTimeField('Start Time')
     end_time = models.DateTimeField('End Time')
     order_created = models.DateTimeField('Created', auto_now_add = True)
-    order_last_modified = models.DateTimeField('Last Modified', auto_now = False)
+    order_last_modified = models.DateTimeField('Last Modified', auto_now = True)
+
+    def __str__(self):
+        start_date = self.start_time.strftime('%Y-%m-%d')
+        return '{} : {}'.format(self.borrower_name, start_date)
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     item = models.ForeignKey(InventoryItem, on_delete=models.CASCADE)
-    quantity_borrowed = models.IntegerField()
-    quantity_returned = models.IntegerField()
+    quantity_borrowed = models.PositiveIntegerField()
+    quantity_returned = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return '{} {}'.format(self.quantity_borrowed, self.item.item_name)
