0c71a9885ff343b4ee7ba8d48fef494b1dc0b212
Aidon Lebar <aidonlebar@gmail.com>
2017-10-13 15:37:47 -0400

    added rudimentary views and html templates

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,4 +5,8 @@ from . import views
 app_name = 'inventory'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^inventory/$', views.inventoryItemIndex, name='inventoryItemIndex'),
+    url(r'^orders/$', views.orderIndex, name='orderIndex'),
+    url(r'^inventoryItem/(?P<inventory_item_id>[0-9]+)/$', views.inventoryDetail, name='inventoryDetail'),
+    url(r'^order/(?P<order_id>[0-9]+)/$', views.orderDetail, name='orderDetail')
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+from .models import InventoryItem, Order, OrderItem
 
 def index(request):
-    return HttpResponse("temp")
+    return render(request, 'inventory/index.html')
+
+def inventoryItemIndex(request):
+    inventory_item_list = InventoryItem.objects.order_by('item_name')
+    context = {'inventory_item_list': inventory_item_list}
+    return render(request, 'inventory/inventoryItemIndex.html', context)
+
+def orderIndex(request):
+    order_list = Order.objects.order_by('start_time')
+    context = {'order_list': order_list}
+    return render(request, 'inventory/orderIndex.html', context)
+
+def inventoryDetail(request, inventory_item_id):
+    inventoryItem = get_object_or_404(InventoryItem, pk=inventory_item_id)
+    return render(request, 'inventory/inventoryDetail.html', {'inventoryItem': inventoryItem})
+
+
+def orderDetail(request, order_id):
+    order = get_object_or_404(Order, pk=order_id)
+    return render(request, 'inventory/orderDetail.html', {'order': order})
