d6a3977c8aa9793f35a0f064509e5c7a5fe824af
Aidon Lebar <aidonlebar@gmail.com>
2017-11-15 22:02:06 -0500

    Serve static files with Django if we're in debug mode

--- plate_club_inventory/urls.py
+++ plate_club_inventory/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
@@ -23,3 +25,6 @@ urlpatterns = [
     url(r'^login/$', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(template_name='inventory/logout.html'), name='logout')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
